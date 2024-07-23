0866e147f4d790e6fe90145d93c31cd1c9bbf0f8
iis istiqomah <iisistiqomah20@gmail.com>
2024-07-23 22:20:03 +0700

    useContext: Cara 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { createContext } from "react";
+import Topbar from "./components/Topbar.js";
 import "./App.css";
-import UseMemoComponent from "./useMemo/index.js";
+
+export const UserContext = createContext({
+  email: "erdecode@gmail.com",
+  name: "Erdecode Channel",
+  id: "123wefdsf4",
+});
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <UseMemoComponent />
+        <Topbar />
       </header>
     </div>
   );
---
+++ src/components/Topbar.js
@@ -0,0 +1,25 @@
+import { UserContext } from "../App";
+
+const Topbar = () => {
+  return (
+    <div>
+      Topbar
+      <UserContext.Consumer>
+        {(values) => {
+          return (
+            <h1>
+              ID : {values.id}
+              <br />
+              Name : {values.name}
+              <br />
+              Email: {values.email}
+              <br />
+            </h1>
+          );
+        }}
+      </UserContext.Consumer>
+    </div>
+  );
+};
+
+export default Topbar;
